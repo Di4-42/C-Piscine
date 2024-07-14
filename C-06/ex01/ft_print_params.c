9f21fdeee3b7aa5cdad7ba7d3e9d9ee9764057fe
Dia <dia.hussain@hotmail.com>
2024-07-14 18:37:14 +0200

    C06

---
+++ C-06/ex01/ft_print_params.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_params.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dihussai <dihussai@student.42lausanne.ch>  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/12 19:16:04 by dihussai          #+#    #+#             */
+/*   Updated: 2024/07/12 19:25:31 by dihussai         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include <unistd.h>
+
+int	main(int argc, char **argv)
+{
+	int	i;
+	int	j;
+
+	(void)argc;
+	i = 1;
+	while (argv[i] != NULL)
+	{
+		j = 0;
+		while (argv[i][j] != '\0')
+		{
+			write (1, &argv[i][j], 1);
+			j++;
+		}
+		i++;
+		write (1, "\n", 1);
+	}
+	return (0);
+}
